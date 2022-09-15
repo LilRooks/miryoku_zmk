1e9eebd32b674c58d99d094ce3c4da0f57a53761
LilRooks <git.ann@rooks.dev>
2022-09-15 19:06:59 -0400

    remove custom layer access

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,16 +6,16 @@
 U_NP,              U_NP,              &kp LCTRL,         &kp SPC,           &kp LALT,          &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 // Direct custom layer access
-#define MIRYOKU_LAYOUTMAPPING_TECHNIKABLE(\
-K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
-K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
-K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
-N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
-) \
-K00  K01  K02  K03  K04  &to U_GAME  &to U_GAME  K05  K06  K07  K08  K09 \
-K10  K11  K12  K13  K14  XXX         XXX         K15  K16  K17  K18  K19 \
-K20  K21  K22  K23  K24  XXX         XXX         K25  K26  K27  K28  K29 \
-          K32  K33  K34  &to U_BASE  &to U_BASE  K35  K36  K37
+//#define MIRYOKU_LAYOUTMAPPING_TECHNIKABLE(\
+//K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09, \
+//K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19, \
+//K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29, \
+//N30, N31, K32, K33, K34,                         K35, K36, K37, N38, N39 \
+//) \
+//K00  K01  K02  K03  K04  &to U_GAME  &to U_GAME  K05  K06  K07  K08  K09 \
+//K10  K11  K12  K13  K14  XXX         XXX         K15  K16  K17  K18  K19 \
+//K20  K21  K22  K23  K24  XXX         XXX         K25  K26  K27  K28  K29 \
+//          K32  K33  K34  &to U_BASE  &to U_BASE  K35  K36  K37
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
